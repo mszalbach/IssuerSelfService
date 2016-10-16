d105088f2966a71ca3c16be037a3e756d8c75225
mszalbach <4-bar-killa@web.de>
2016-10-16 11:11:53 +0200

    - added server module with simple rest service

---
+++ server/src/main/java/de/blogspot/mszalbach/iss/Application.java
@@ -0,0 +1,15 @@
+package de.blogspot.mszalbach.iss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by foobarkilla on 16.10.16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
