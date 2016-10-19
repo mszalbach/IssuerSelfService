e2035b801e8849699cc2ad12320998ad65779134
mszalbach <4-bar-killa@web.de>
2016-10-19 15:29:08 +0200

    - added proxy config so the spring application can be reached via /api for the client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const merge = require( 'webpack-merge' );
-const validate = require( 'webpack-validator' );
-const pkg = require( "./package.json" );
-const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const pkg = require("./package.json");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const PATHS = {
-    app: path.join( __dirname, 'src/main/js' ),
-    build: path.join( __dirname, 'target/build' )
+    app: path.join(__dirname, 'src/main/js'),
+    build: path.join(__dirname, 'target/build')
 };
 
 
 const common = {
     entry: {
         app: PATHS.app,
-        vendor: Object.keys( pkg.dependencies )
+        vendor: Object.keys(pkg.dependencies)
     },
     devtool: 'source-map',
     output: {
@@ -33,7 +33,7 @@ const common = {
     module: {
         loaders: [
             {test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader'},
-            {test: /\.css$/, loader: ExtractTextPlugin.extract( 'style-loader', 'css-loader' )},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
             //needed for bootstrap
             {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
@@ -42,12 +42,24 @@ const common = {
         ]
     },
 
+    devServer: {
+        proxy: {
+            '/api/**': {
+                target: 'http://localhost:8081',
+                rewrite: function (req) {
+                    req.url = req.url.replace(/^\/api(.+)$/, '$1');
+                }
+            }
+
+        }
+    },
+
 
     plugins: [
-        new ExtractTextPlugin( 'bundle.css' ),
-        new HtmlWebpackPlugin( {
+        new ExtractTextPlugin('bundle.css'),
+        new HtmlWebpackPlugin({
             template: './src/main/js/index.html'
-        } )
+        })
     ]
 };
 
@@ -55,34 +67,34 @@ const common = {
 var config;
 
 // Detect how npm is run and branch based on that
-switch ( process.env.npm_lifecycle_event ) {
+switch (process.env.npm_lifecycle_event) {
     case 'build':
-        config = merge( common, {
+        config = merge(common, {
             output: {
                 filename: '[name].[chunkhash].js'
             },
             devtool: 'cheap-source-map',
-            plugins: [new webpack.DefinePlugin( {
-                'process.env.NODE_ENV': JSON.stringify( 'production' )
+            plugins: [new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
 
-            } ),
-                new webpack.optimize.UglifyJsPlugin( {
+            }),
+                new webpack.optimize.UglifyJsPlugin({
                     compress: {
                         warnings: false
                     }
-                } ),
-                new webpack.optimize.CommonsChunkPlugin( "vendor", "[name].[chunkhash].js" ),
-                new CleanWebpackPlugin( PATHS.build )
+                }),
+                new webpack.optimize.CommonsChunkPlugin("vendor", "[name].[chunkhash].js"),
+                new CleanWebpackPlugin(PATHS.build)
             ]
 
-        } );
+        });
         break;
     default:
-        config = merge( common, {
+        config = merge(common, {
                 devtool: 'eval-source-map'
             }
         );
 
 }
 
-module.exports = validate( config );
+module.exports = validate(config);
