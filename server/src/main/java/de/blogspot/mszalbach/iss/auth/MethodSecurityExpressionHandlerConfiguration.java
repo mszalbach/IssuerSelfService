50b52e8544c8295b748883c31085a9dd1574b9ff
mszalbach <4-bar-killa@web.de>
2016-12-06 08:48:07 +0100

    - added SecurityChecker to check if an security expression is true

---
+++ server/src/main/java/de/blogspot/mszalbach/iss/auth/MethodSecurityExpressionHandlerConfiguration.java
@@ -0,0 +1,18 @@
+package de.blogspot.mszalbach.iss.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+
+/**
+ * Created by ms on 06.12.16.
+ */
+@Configuration
+public class MethodSecurityExpressionHandlerConfiguration {
+
+    @Bean
+    public MethodSecurityExpressionHandler expressionHandler() {
+        return new DefaultMethodSecurityExpressionHandler();
+    }
+}
---
+++ server/src/main/java/de/blogspot/mszalbach/iss/auth/SecurityChecker.java
@@ -0,0 +1,55 @@
+package de.blogspot.mszalbach.iss.auth;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.security.access.expression.ExpressionUtils;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.util.SimpleMethodInvocation;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by ms on 05.12.16.
+ */
+@Service
+public class SecurityChecker {
+
+    @Autowired
+    MethodSecurityExpressionHandler expressionHandler;
+
+    static Logger logger = Logger.getLogger( SecurityChecker.class );
+
+    private static class SecurityObject {
+
+        public void triggerCheck() { /*NOP*/ }
+    }
+
+    private static Method               triggerCheckMethod;
+    private static SpelExpressionParser parser;
+
+    static {
+        try {
+            triggerCheckMethod = SecurityObject.class.getMethod( "triggerCheck" );
+        } catch ( NoSuchMethodException e ) {
+            logger.error( e );
+        }
+        parser = new SpelExpressionParser();
+    }
+
+    public boolean check( String securityExpression ) {
+
+        SecurityObject securityObject = new SecurityObject();
+        EvaluationContext evaluationContext = expressionHandler
+                .createEvaluationContext( SecurityContextHolder.getContext().getAuthentication(),
+                                          new SimpleMethodInvocation( securityObject, triggerCheckMethod ) );
+        boolean checkResult = ExpressionUtils
+                .evaluateAsBoolean( parser.parseExpression( securityExpression ), evaluationContext );
+
+        return checkResult;
+    }
+
+}
