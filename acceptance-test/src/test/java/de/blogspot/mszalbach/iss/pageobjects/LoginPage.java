fb81d311ec5812ccbb4aa516d105fffdb325bf0e
mszalbach <4-bar-killa@web.de>
2016-12-09 16:00:52 +0100

    - renamed fields

--- acceptance-test/src/test/java/de/blogspot/mszalbach/iss/pageobjects/LoginPage.java
+++ acceptance-test/src/test/java/de/blogspot/mszalbach/iss/pageobjects/LoginPage.java
@@ -15,8 +15,8 @@ public class LoginPage
 
     public static Target USER_FIELD     = Target.the( "Username Field" ).locatedBy( "#user" );
     public static Target PASSWORD_FIELD = Target.the( "Password Field" ).locatedBy( "#password" );
-    public static Target LOGIN_BUTTON   = Target.the( "Password Field" ).locatedBy( "#login" );
-    public static Target ERROR_TEXT   = Target.the( "Password Field" ).locatedBy( "#loginError" );
+    public static Target LOGIN_BUTTON   = Target.the( "Login Button" ).locatedBy( "#login" );
+    public static Target ERROR_TEXT   = Target.the( "Login Error Text" ).locatedBy( "#loginError" );
 
 
 
